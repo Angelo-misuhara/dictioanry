5013ae35d61c8ef7f11aafc010ffe92f815af5e8
ogel <yocorangelo18@gmail.com>
2023-07-23 21:00:02 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Result from './components/Result';
+import axios from 'axios';
+import Footer from './components/Footer';
+
+function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+  const [audio, setAudio] = useState('');
+  const [word, setWord] = useState('');
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+
+  const fetchData = () => {
+    axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${search}`)
+      .then((response) => {
+        const data = response.data;
+        if (Array.isArray(data) && data.length > 0) {
+          setData(data[0].meanings);
+          setAudio(data[0].phonetics[0].audio);
+          setWord(data[0].word);
+          setSearch('')
+        } else {
+          setData([]);
+          setAudio('');
+          setWord('');
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div className='flex flex-col items-center w-screen mt-32 flex-wrap'>
+        <div className='m-5'>
+          <h1 className='font-semibold text-3xl'>D-App</h1>
+        </div>
+        <div className='flex justify-center m-5'>
+          <form onSubmit={handleFormSubmit} className='flex flex-row'>
+            <input
+              type='text'
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className='w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+            />
+            <button type='submit' className='ml-3 border px-3 bg-amber-400 hover:bg-amber-500 rounded-md'>
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+      <Result data={data} word={word} audio={audio} />
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Result.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/prop-types */
+import uuid from 'react-uuid';
+
+// eslint-disable-next-line react/prop-types
+const Result = ({ data, word ,audio  }) => {
+  
+
+  console.log(data)
+  const playSound = () => {
+    new Audio(audio).play()
+  }
+  return (
+    <>
+      <div className='grid grid-cols-2 grid-flow-col gap-1 ' id='colRes'>
+        <div className=' p-6  justify-items-center justify-self-center'>
+          <h1>Word: <strong>{word.toUpperCase()}</strong></h1>
+          <div className='mt-5'>
+            <button className='text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg' onClick={playSound}>{audio === '' && <h1>No Audio available</h1> || <h1> play</h1>}</button>
+          </div>
+        </div>
+        <div className=' p-2 justify-self-center justify-items-center'>
+          <ul>
+            <h1 className='font-extrabold m-3'>Definitions</h1>
+            {data.map((meaning) => {
+              return (
+                <>
+                  <li className='text-start font-semibold p-1 list-disc list-inside' key={uuid()}>
+                    { meaning.definitions[0].definition}
+                  </li>
+                </>
+              )
+            }
+            )}
+            </ul>
+          </div>
+    </div>
+    </>
+  )
+}
+
+export default Result
