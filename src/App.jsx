9a0228cfd2ce2fce4606d804a1751b4a25e84592
ogel <yocorangelo18@gmail.com>
2023-07-23 21:28:33 +0800

    2v

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from './components/Footer';
 
 function App() {
   const [data, setData] = useState([]);
-  const [search, setSearch] = useState('hello');
+  const [search, setSearch] = useState('cat');
   const [audio, setAudio] = useState('');
   const [word, setWord] = useState('');
 
